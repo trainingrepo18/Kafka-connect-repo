6f3c592a0ab64f2b9005caea7b243f4c674d6791
trainingrepo18 <106578784+trainingrepo18@users.noreply.github.com>
2024-08-08 22:50:48 +0530

    Update 00_setup_db.sql

--- kafka-connect-zero-to-hero/data/mysql/00_setup_db.sql
+++ kafka-connect-zero-to-hero/data/mysql/00_setup_db.sql
@@ -1,9 +1,9 @@
-CREATE USER 'debezium'@'%' IDENTIFIED WITH mysql_native_password BY 'dbz';
-CREATE USER 'replicator'@'%' IDENTIFIED BY 'replpass';
+CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
+CREATE USER 'root'@'%' IDENTIFIED BY 'replpass';
 
-GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'debezium';
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'root';
 GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator';
 
 create database demo;
 
-GRANT ALL PRIVILEGES ON demo.* TO 'debezium'@'%';
+GRANT ALL PRIVILEGES ON demo.* TO 'root'@'%';
